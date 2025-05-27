1066111bddf0d84ff84254053b87e27bcd361332
Ethan Tran <tran.ethan.000@gmail.com>
2025-05-26 21:14:46 -0700

    V0.0.2 Infinite Void svg and icon added

--- Domain-Type/src/Components/Nav.jsx
+++ Domain-Type/src/Components/Nav.jsx
@@ -1,12 +1,13 @@
 import '../App.css'
+import infiniteVoid from '../imgs/infiniteVoid.svg';
 
 export default function Nav() {
     return (
-        <nav className="flex justify-between items-center bg-[#0B4C6A] py-12 px-16">
-            <div className='flex flex-row'>
+        <nav className="flex justify-between items-center bg-[#0B4C6A] py-6 px-16">
+            <div className='flex flex-row items-center'>
                 <h1 className='text-3xl text-white font-medium'>Domain Type</h1>
-                <div className='justify-around flex'>
-                    <img src="infiniteVoid.svg" alt="practice button icon" />
+                <div className='justify-around flex items-center'>
+                    <img src={infiniteVoid} alt="Infinite Void" className='w-20 mt-4'/>
                     {/* <img src="fuga.svg" alt="Player vs player button icon" />
                     <img src="shrine.svg" alt="Account stats button icon" /> */}
                     <img src="gear.svg" alt="Settings button icon" />
